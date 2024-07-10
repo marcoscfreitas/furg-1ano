5eb4d93b57ed9b143f79bd114e55719bea050551
Marcos Vinicius <84063065+marcoscfreitas@users.noreply.github.com>
2024-07-10 14:46:53 +0000

    primeiras aulas aed1 e si

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # furg-1semestre
+Repositório para aulas do primeiro semestre de sistemas de informação na FURG.
---
+++ algoritimos e estruturas de dados 1/alomundo.py
@@ -0,0 +1,10 @@
+print('Alô mundo dos Algoritmos!')
+print('Eu amo o Prisco\n' * 10)
+x = 7
+print(type(x)) #class 'int'
+print(x)
+x = 'e agora?'
+print(type(x)) #class 'str'
+print(x)
+nome = input('digite seu nome: ')
+print('olá, ' + nome)
