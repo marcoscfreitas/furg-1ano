7ab428d258cf0b96f9f2903017249c41a68da7ee
Marcos Vinicius <84063065+marcoscfreitas@users.noreply.github.com>
2024-07-21 02:31:49 +0000

    término exercicio do triangulo

--- algoritmos e estruturas de dados 1/exercicio8.py
+++ algoritmos e estruturas de dados 1/exercicio8.py
@@ -6,14 +6,16 @@ lado3 = int(input('informe lado 3: '))
 soma1 = lado2 + lado3
 soma2 = lado1 + lado3
 soma3 = lado1 + lado2
+print(lado1,lado2,lado3)
 print(soma1,soma2,soma3)
 
-if soma1 < lado1 or soma2 < lado2 or soma3 < lado3 :
+if soma1 <= lado1 or soma2 <= lado2 or soma3 <= lado3 :
 	print('não é um triangulo válido')
 else :
 	if lado1 == lado2 and lado2 == lado3 and lado1 == lado3 :	
 		print('é triangulo EQUILATERO')
-	else lado1 == lado2 or lado2 == lado3 or lado1 == lado3 :
-		print('é triangulo ISOCELES')
 	else :
-		print('é triangulo ESCALENO')
+		if lado1 == lado2 or lado2 == lado3 or lado1 == lado3 :
+			print('é triangulo ISOCELES')
+		else :
+			print('é triangulo ESCALENO')
