1538e918a12b68372343012484997327ddbe4a4e
Marcos Vinicius <84063065+marcoscfreitas@users.noreply.github.com>
2024-07-24 18:10:42 +0000

    término exercicios if e else

--- algoritmos e estruturas de dados 1/lista1_aed1.py
+++ algoritmos e estruturas de dados 1/lista1_aed1.py
@@ -1,45 +1,117 @@
 # 1 Faça um programa que, ao receber os valores da largura e do comprimento de uma figura geométrica, mostre se esta é um quadrado ou apenas um retângulo. Caso um valor seja menor ou igual a zero, deve-se mostrar um erro.
+'''
+largura = int(input('informe a largura da figura: '))
+comprimento = int(input('informe o comprimento da figura: '))
 
+if largura <= 0 or comprimento <= 0 :
+    print('por favor, informe valores válidos')
+else :
+    if largura == comprimento :
+        print('é um quadrado')
+    else : 
+        print('é um retângulo')
+'''  
 # 2 Faça um programa em python que pergunte ao usuário o seguinte:
-#	A viagem custará menos de R$ 30?
-#	Terá Wifi?
-#	Terá piscina?
-#	Terá churrasqueira?
+#	- A viagem custará menos de R$ 30?
+#	- Terá Wifi?
+#	- Terá piscina?
+#	- Terá churrasqueira?
 #		O programa deverá mostrar se a viagem ocorrerá de acordo com as seguintes regras:
-#	Deverá custar menos de R$ 30
-#	Tem que ter wifi e piscina
-#	Se não tiver wifi ou piscina, tem que ter churrasqueira
+#			- Deverá custar menos de R$ 30
+#			- Tem que ter wifi e piscina
+#			- Se não tiver wifi ou piscina, tem que ter churrasqueira
+'''
+valor = int(input('a viagem custará menos de R$30,00?\n1 - SIM\n2 - NÃO\n'))
+wifi = int(input('terá wifi?\n1 - SIM\n2 - NÃO\n'))
+piscina = int(input('terá piscina?\n1 - SIM\n2 - NÃO\n'))
+churras = int(input('terá churrasqueira?\n1- SIM\n2 - NÃO\n'))
 
+if valor == 2 :
+    print('a viagem não ocorrerá')
+else :
+    if churras == 1 :
+        print('a viagem ocorrerá')
+    else :
+        if wifi == 1 and piscina == 1 :
+            print('a viagem ocorrerá')
+        else :
+            print('a viagem não ocorrerá')
+'''
 # 3 Construa um programa em python que, informadas três medidas a, b e c pelo usuário, verifique se elas podem ser lados de um triângulo. Se não puderem ser, primeiramente o algoritmo deve informar isso. Se for possível serem lados de triângulo, deve dizer qual tipo de triângulo pode ser construído com essas medidas (isósceles, escaleno ou equilátero). A condição para formar um triângulo: comprimento do maior segmento seja inferior à soma dos comprimentos dos dois menores. 
+'''
+lado1 = int(input('informe lado 1: '))
+lado2 = int(input('informe lado 2: '))
+lado3 = int(input('informe lado 3: '))
+soma1 = lado2 + lado3
+soma2 = lado1 + lado3
+soma3 = lado1 + lado2
+print(lado1,lado2,lado3)
+print(soma1,soma2,soma3)
 
+if soma1 <= lado1 or soma2 <= lado2 or soma3 <= lado3 :
+	print('não é um triangulo válido')
+else :
+	if lado1 == lado2 and lado2 == lado3 and lado1 == lado3 :	
+		print('é triangulo EQUILATERO')
+	else :
+		if lado1 == lado2 or lado2 == lado3 or lado1 == lado3 :
+			print('é triangulo ISOCELES')
+		else :
+			print('é triangulo ESCALENO')
+'''
 # 4 Crie um programa em Python que leia as notas do estudante nos 4 bimestres da nossa disciplina e a frequência (em porcentagem). A seguir informe se o estudante passou por média, rodou ou ficou em exame. Para passar por média, o estudante deve ter média maior ou igual a 7. Estudante com média abaixo de 3 roda sem ao menos fazer o exame. O estudante que tiver menos de 75% de frequência também está rodado na disciplina.
+'''
+nota1 = float(input('informe a nota do 1º bimestre: '))
+nota2 = float(input('informe a nota do 2º bimestre: '))
+nota3 = float(input('informe a nota do 3º bimestre: '))
+nota4 = float(input('informe a nota do 4º bimestre: '))
+freq = float(input('informe a frequência do estudante: '))
+media = (nota1 + nota2 + nota3 + nota4)/4
 
+if freq < 75 :
+	print('o estudante rodou por frequência')
+else :
+	if media < 3 :
+		print('o estudante rodou direto, média:',media)
+	else :
+		if media >= 3 and media < 7 :
+			print('o estudante ficou em exame, média:',media)
+		else :
+			print('o estudante passou por média')
+'''
 # 5 Crie um programa em Python que leia um número e diga se ele é par ou ímpar.
+'''
+num = int(input('informe um número inteiro: '))
 
+if num % 2 == 0 :
+    print(f'o número {num} é PAR.')
+else :
+    print(f'o número {num} é ÍMPAR')
+'''
 # 6 Faça um programa em python que leia 3 números e os mostre em ordem crescente.
 '''
 num1 = int(input('informe um número: '))
 num2 = int(input('informe um número: '))
 num3 = int(input('informe um número: '))
 
-if num1 >= num2 and num1 >= num3 and num2 >= num3 :
-	print(num3,num2,num1)
-else :
-	if num1 >= num2 and num1 >= num3 and num3 >= num2 :
+if num1 >= num2 and num1 >= num3 :
+	if num3 >= num2 :
 		print(num2,num3,num1)
 	else :
-		if num2 >= num1 and num2 >= num3 and num1 >= num3 :
-			print(num3,num1,num2)
+		print(num3,num2,num1)
+else :
+	if num2 >= num1 and num2 >= num3 :
+		if num3 >= num1 :
+			print(num1,num3,num2)
 		else :
-			if num2 >= num1 and num2 >= num3 and num3 >= num1 :
-				print(num1,num3,num2)
+			print(num3,num1,num2)
+	else : 
+		if num3 >= num1 and num3 >= num2 :
+			if num2 >= num1 :
+				print(num1,num2,num3)
 			else :
-				if num3 >= num1 and num3 >= num2 and num1 >= num2 :
-					print(num2,num1,num3)
-				else :
-					if num3 >= num1 and num3 >= num2 and num2 >= num1 :
-						print(num1,num2,num3)
-'''
+				print(num2,num1,num3)
+'''	
 # 7 Crie um programa em Pyhon que leia uma data (DD/MM/AAAA) e diga se a data é válida. a) Desconsidere anos bissextos. b) Considere anos bissextos
 '''
 # SEM ANO BISSEXTO
@@ -57,8 +129,7 @@ else :
 		if dia == 31 and mes != 1 and mes != 3 and mes != 5 and mes != 7 and mes != 8 and mes != 10 and mes != 12 :
 			print('não é válido')
 		else :
-			print('é válido')
-			
+			print('é válido')	
 '''
 '''
 # COM ANO BISSEXTO
@@ -124,24 +195,59 @@ else :
 	print(nomeY,'vence o campeonato, com',Ty,'gols')
 '''
 # 9 Crie um programa em Python que leia o rendimento mensal do usuário, qual o modelo de imposto (sem correção/com correção das perdas no governo Bolsonaro) e retorne o quanto ele deve pagar de imposto.
-
+'''
 rendM = int(input('informe o seu rendimento mensal: '))
 mod = int(input('qual o modelo de imposto?\n1 - SEM CORREÇÃO\n2 - COM CORREÇÃO DAS PERDAS BOLSONARO\n3 - COM CORREÇÃO INTEGRAL\n'))
 if mod == 1 :
-	if rendM <= 1904 :
-		print('isento')
+	if rendM < 1904 :
+		print('você está isento de impostos')
 	else:
-		if rendM >= 1904 and <= 2827 :
-		print('7.50%')
-		if rendM >= 2827 and <= 3751 :
-		print('15.00%')
-
-
-
-
-
-
-
+		if rendM >= 1904 and rendM<= 2827 :
+			imp = (rendM*7.50)/100
+			print('você deve pagar 7.50% do seu rendimento de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 2827 and  rendM <= 3751 :
+			imp = (rendM*15.00)/100
+			print('você deve pagar 15.00% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 3751 and rendM <= 4665 :
+			imp = (rendM*22.50)/100
+			print('você deve pagar 22.50% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 4665 :
+			imp = (rendM*27.50)/100
+			print('você deve pagar 27.50% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+if mod == 2 :
+	if rendM < 2500 :
+		print('você está isento de impostos')
+	else:
+		if rendM >= 2500 and rendM<= 3712 :
+			imp = (rendM*7.50)/100
+			print('você deve pagar 7.50% do seu rendimento de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 3712 and  rendM <= 4926 :
+			imp = (rendM*15.00)/100
+			print('você deve pagar 15.00% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 4926 and rendM <= 6126 :
+			imp = (rendM*22.50)/100
+			print('você deve pagar 22.50% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 6126 :
+			imp = (rendM*27.50)/100
+			print('você deve pagar 27.50% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+if mod == 3 :
+	if rendM < 4710 :
+		print('você está isento de impostos')
+	else:
+		if rendM >= 4710 and rendM<= 6993 :
+			imp = (rendM*7.50)/100
+			print('você deve pagar 7.50% do seu rendimento de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 6993 and  rendM <= 9208 :
+			imp = (rendM*15.00)/100
+			print('você deve pagar 15.00% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 9208 and rendM <= 11540 :
+			imp = (rendM*22.50)/100
+			print('você deve pagar 22.50% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+		if rendM >= 11540 :
+			imp = (rendM*27.50)/100
+			print('você deve pagar 27.50% do seu rendiment de imposto, ou seja:\nvocê pagará: R$:',imp)
+'''
+#------------------------- repetição -------------------------
 
 # 10 Escreva um programa que mostre os números de 1 a 10.
 
@@ -156,7 +262,7 @@ if mod == 1 :
 #   1
 #   2 2
 #   3 3 3
-#  4 4 4 4
+#  	4 4 4 4
 #   5 5 5 5 5
 #   …
 #   N N N N N N N …
@@ -194,11 +300,11 @@ if mod == 1 :
 # Ex: 	Informe o tijolo: A
 # 	Informe a quantidade de andares: 5
 
-#        A
-#      AAA
-#    AAAAA
-#  AAAAAAA
-#AAAAAAAAA
+#         A
+#       AAA
+#     AAAAA
+#   AAAAAAA
+# AAAAAAAAA
 
 
 
