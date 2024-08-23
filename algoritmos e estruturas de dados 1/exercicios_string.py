b5379905fa1081f0b716e14578e9fbe15f6bd7a7
Marcos Vinicius <84063065+marcoscfreitas@users.noreply.github.com>
2024-08-23 13:40:57 +0000

    aula strings 23/09

--- algoritmos e estruturas de dados 1/exercicios_string.py
+++ algoritmos e estruturas de dados 1/exercicios_string.py
@@ -64,3 +64,82 @@ print(x)
 print('----------------')
 print(final)
 '''
+# ler um nome e mostrar um por um
+'''
+nome = input('informe o nome completo: ')
+cont = 0
+final = ''
+
+while cont < len(nome) :
+    char = nome[cont]
+    final = final + char
+    if char == ' ' or cont == len(nome)-1 :
+        print(final)
+        final = ''
+        cont+=1
+    else :
+        cont+=1
+'''
+# ler um nome completo em minúsculo e capitalizar a primeira letra, menos os "de"
+'''
+nome = input('informe um nome completo: ')
+cont = 0
+final = ''
+resultado = ''
+
+while cont < len(nome) :
+    char = nome[cont]
+    final = final + char
+    if char == ' ' or cont == len(nome)-1 :
+        final = final[0].upper() + final
+        resultado =  resultado + final
+        final = ''
+    cont+=1
+print(resultado)
+'''
+nome = input('informe um nome completo: ')
+cont = 0
+parcial = ''
+final = ''
+while cont < len(nome) :
+    if len(parcial) == 0:
+        char = nome[cont].upper()
+        parcial = parcial + char
+        cont+=1
+    if len(parcial) > 0:
+        char = nome[cont].lower()
+        parcial = parcial + char
+    if char == ' ' or cont == len(nome)-1 :
+        if len(parcial) <= 4:
+            final = final + parcial.lower()
+            parcial = ''
+        else :
+            final =  final + parcial
+            parcial = ''
+    cont+=1
+print(final, parcial)
+
+#leia um nome e coloque as letras minusculuas
+'''
+nome = input('digite um nome: ')
+indice = 0
+while indice < len(nome) :
+    letra = nome[indice]
+    codigo = ord[letra]
+    print(codigo, letra)
+    indice = indice + 1
+'''
+'''
+nome = input('digite um nome: ')
+indice = 0
+nome_minusculo = ''
+while indice < len(nome) :
+    letra = nome[indice]
+    codigo = ord[letra]
+    minusculo = codigo + 32
+    #print(codigo,letra,minusculo,chr(minusculo))
+    nome_minusculo = nome_minusculo + chr(minusculo)
+    incide = indice + 1
+
+print(nome_minusculo)
+'''
